d8610b6ef18db6a858439120c961daeb0702860d
AIlox <148843429+e-ALX-e@users.noreply.github.com>
2026-09-21 17:59:24 +0800

    feat: export translation rating schema

--- db/schema.ts
+++ db/schema.ts
@@ -7,4 +7,4 @@ export {
   siteCommentTargetTypeEnum,
 } from './schema/comment'
 export { friendLinkStateEnum, friendLinks } from './schema/friend-link'
-export { blogTranslations, translationModelConfig, translationTasks, translationUsage } from './schema/translation'
+export { blogTranslations, translationModelConfig, translationRatings, translationTasks, translationUsage } from './schema/translation'

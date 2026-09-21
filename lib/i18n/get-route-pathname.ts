8c17047cfd386241a59bf07b017deb42db2bf6ac
AIlox <148843429+e-ALX-e@users.noreply.github.com>
2026-09-21 14:34:48 +0800

    fix: recognize all locale prefixes

--- lib/i18n/get-route-pathname.ts
+++ lib/i18n/get-route-pathname.ts
@@ -1,5 +1,5 @@
 export function getRoutePathname(pathname: string) {
-  const routePathname = pathname.replace(/^\/(?:zh|en)(?=\/|$)/, '')
+  const routePathname = pathname.replace(/^\/(?:zh|en|zh-tw|ja|ru|de)(?=\/|$)/, '')
 
   return routePathname === '' ? '/' : routePathname
 }
